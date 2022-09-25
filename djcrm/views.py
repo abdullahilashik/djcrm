1621b705bed1b801f9ab0a005bbae813fabb87f1
Abdullahil Ashik Md. Arefin <abdullah.ashik.arefin@gmail.com>
2022-09-26 00:15:12 +0600

    template

--- djcrm/views.py
+++ djcrm/views.py
@@ -3,8 +3,4 @@ from django.shortcuts import render
 
 def home(request):
     context = {}
-    return render(
-        request,
-        'pages/home.html',
-        context
-    )
+    return render(request,'pages/home.html',context)
